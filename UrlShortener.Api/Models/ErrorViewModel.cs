cb9d985a23116f7932078ca25be21636a7ab8b39
lexamania <torbanowmain@gmail.com>
2026-01-22 02:38:27 +0200

    initialize api + front

---
+++ UrlShortener.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UrlShortener.Api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
