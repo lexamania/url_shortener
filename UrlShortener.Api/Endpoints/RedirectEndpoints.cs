da7278297083059424e27dd7b5d063208b7f22e3
lexamania <torbanowmain@gmail.com>
2026-02-12 16:07:10 +0200

    make redirection

--- UrlShortener.Api/Endpoints/RedirectEndpoints.cs
+++ UrlShortener.Api/Endpoints/RedirectEndpoints.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using UrlShortener.Api.Data;
+
 namespace UrlShortener.Api.Endpoints;
 
 public static class RerouteEndpoints
@@ -5,6 +10,17 @@ public static class RerouteEndpoints
     public static WebApplication MapRedirectEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("/");
+        group.MapGet("/{shortUrl}", RedirectByShortUrl);
         return app;
+    }
+
+    public static async Task<IResult> RedirectByShortUrl(
+        [FromRoute] string shortUrl,
+        [FromServices] UrlShortenerDbContext dbContext)
+    {
+        var url = await dbContext.Urls.FirstOrDefaultAsync(x => x.ShortUrl == shortUrl);
+        return url is null
+            ? TypedResults.Problem("Url not found", statusCode: StatusCodes.Status404NotFound)
+            : Results.Redirect(url.Url);
     }
 }
